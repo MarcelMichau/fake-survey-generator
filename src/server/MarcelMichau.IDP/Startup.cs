8e51485cd6435861ea47286de3772918fe7b93a3
Marcel Michau <marcelmichau@gmail.com>
2019-09-18 20:59:11 +0200

    feat(auth): local https make everything on local https add certs & secrets for k8s.local & identity.k8s.local add commands to generate local certs trying out this legendary stackoverflow solution

--- src/server/MarcelMichau.IDP/Startup.cs
+++ src/server/MarcelMichau.IDP/Startup.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
@@ -62,9 +66,11 @@ namespace MarcelMichau.IDP
             {
                 options.ForwardedHeaders =
                     ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-                options.KnownNetworks.Clear();
-                options.KnownProxies.Clear();
-                //options.KnownProxies.Add(IPAddress.Parse("10.244.1.1"));
+
+                foreach (var network in GetNetworks(NetworkInterfaceType.Ethernet))
+                {
+                    options.KnownNetworks.Add(network);
+                }
             });
         }
 
@@ -119,6 +125,53 @@ namespace MarcelMichau.IDP
             app.UseRouting();
 
             app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
+        }
+
+        private static IEnumerable<IPNetwork> GetNetworks(NetworkInterfaceType type)
+        {
+
+            foreach (var item in NetworkInterface.GetAllNetworkInterfaces()
+                .Where(n => n.NetworkInterfaceType == type && n.OperationalStatus == OperationalStatus.Up)  // get all operational networks of a given type
+                .Select(n => n.GetIPProperties())   // get the IPs
+                .Where(n => n.GatewayAddresses.Any())) // where the IPs have a gateway defined
+            {
+                var ipInfo = item.UnicastAddresses.FirstOrDefault(i => i.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork); // get the first cluster-facing IP address
+                if (ipInfo == null) { continue; }
+
+                // convert the mask to bits
+                var maskBytes = ipInfo.IPv4Mask.GetAddressBytes();
+                if (!BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(maskBytes);
+                }
+                var maskBits = new BitArray(maskBytes);
+
+                // count the number of "true" bits to get the CIDR mask
+                var cidrMask = maskBits.Cast<bool>().Count(b => b);
+
+                // convert my application's ip address to bits
+                var ipBytes = ipInfo.Address.GetAddressBytes();
+                if (!BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(maskBytes);
+                }
+                var ipBits = new BitArray(ipBytes);
+
+                // and the bits with the mask to get the start of the range
+                var maskedBits = ipBits.And(maskBits);
+
+                // Convert the masked IP back into an IP address
+                var maskedIpBytes = new byte[4];
+                maskedBits.CopyTo(maskedIpBytes, 0);
+                if (!BitConverter.IsLittleEndian)
+                {
+                    Array.Reverse(maskedIpBytes);
+                }
+                var rangeStartIp = new IPAddress(maskedIpBytes);
+
+                // return the start IP and CIDR mask
+                yield return new IPNetwork(rangeStartIp, cidrMask);
+            }
         }
     }
 }
