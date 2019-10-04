cd0ec8caff60acd2f9f28c6cb9a222b434cc9c0d
Marcel Michau <Marcel.Michau@investec.co.za>
2019-10-04 08:58:06 +0200

    feat(migrations): add idp ef design project update idp pipeline to create all db migration scripts

---
+++ src/server/MarcelMichau.IDP.EF.Design/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarcelMichau.IDP.EF.Design
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("This project exists for the sole purpose of EF Core Design-Time tooling");
+        }
+    }
+}
