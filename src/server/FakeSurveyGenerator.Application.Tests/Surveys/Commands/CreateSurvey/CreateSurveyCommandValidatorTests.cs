8ca1a0046ed434b6a0b64a7d3e65e1e562c2db38
Marcel Michau <marcelmichau@gmail.com>
2020-01-27 20:46:41 +0200

    fix(tests)
    add create survey command validator test for survey option text
    check for correct property being validated in create survey command validator tests

--- src/server/FakeSurveyGenerator.Application.Tests/Surveys/Commands/CreateSurvey/CreateSurveyCommandValidatorTests.cs
+++ src/server/FakeSurveyGenerator.Application.Tests/Surveys/Commands/CreateSurvey/CreateSurveyCommandValidatorTests.cs
@@ -29,37 +29,61 @@ namespace FakeSurveyGenerator.Application.Tests.Surveys.Commands.CreateSurvey
         [Fact]
         public void IsValid_ShouldBeFalse_WhenSurveyTopicIsBlank()
         {
-            var command = new CreateSurveyCommand("", 1, "Test", new List<SurveyOptionDto>());
+            var command = new CreateSurveyCommand("", 1, "Test", new List<SurveyOptionDto>
+            {
+                new SurveyOptionDto
+                {
+                    OptionText = "Test",
+                    PreferredNumberOfVotes = 1
+                }
+            });
 
             var validator = new CreateSurveyCommandValidator();
 
             var result = validator.Validate(command);
 
             result.IsValid.ShouldBe(false);
+            result.Errors.ShouldContain(error => error.PropertyName == nameof(CreateSurveyCommand.SurveyTopic));
         }
 
         [Fact]
         public void IsValid_ShouldBeFalse_WhenNumberOfRespondentsIsZero()
         {
-            var command = new CreateSurveyCommand("Test", 0, "Test", new List<SurveyOptionDto>());
+            var command = new CreateSurveyCommand("Test", 0, "Test", new List<SurveyOptionDto>
+            {
+                new SurveyOptionDto
+                {
+                    OptionText = "Test",
+                    PreferredNumberOfVotes = 1
+                }
+            });
 
             var validator = new CreateSurveyCommandValidator();
 
             var result = validator.Validate(command);
 
             result.IsValid.ShouldBe(false);
+            result.Errors.ShouldContain(error => error.PropertyName == nameof(CreateSurveyCommand.NumberOfRespondents));
         }
 
         [Fact]
         public void IsValid_ShouldBeFalse_WhenRespondentTypeIsNotSpecified()
         {
-            var command = new CreateSurveyCommand("Test", 1, "", new List<SurveyOptionDto>());
+            var command = new CreateSurveyCommand("Test", 1, "", new List<SurveyOptionDto>
+            {
+                new SurveyOptionDto
+                {
+                    OptionText = "Test",
+                    PreferredNumberOfVotes = 1
+                }
+            });
 
             var validator = new CreateSurveyCommandValidator();
 
             var result = validator.Validate(command);
 
             result.IsValid.ShouldBe(false);
+            result.Errors.ShouldContain(error => error.PropertyName == nameof(CreateSurveyCommand.RespondentType));
         }
 
         [Fact]
@@ -72,6 +96,26 @@ namespace FakeSurveyGenerator.Application.Tests.Surveys.Commands.CreateSurvey
             var result = validator.Validate(command);
 
             result.IsValid.ShouldBe(false);
+            result.Errors.ShouldContain(error => error.PropertyName == nameof(CreateSurveyCommand.SurveyOptions));
+        }
+
+        [Fact]
+        public void IsValid_ShouldBeFalse_WhenSurveyOptionTextIsEmpty()
+        {
+            var command = new CreateSurveyCommand("Test", 1, "Test", new List<SurveyOptionDto>
+            {
+                new SurveyOptionDto
+                {
+                    OptionText = ""
+                }
+            });
+
+            var validator = new CreateSurveyCommandValidator();
+
+            var result = validator.Validate(command);
+
+            result.IsValid.ShouldBe(false);
+            result.Errors.ShouldContain(error => error.PropertyName == "SurveyOptions[0].OptionText");
         }
     }
 }
