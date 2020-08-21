e4fa4172429cc6b51ad5f7bac1b4a64ea9423067
Marcel Michau <marcelmichau@gmail.com>
2020-08-21 15:36:16 +0200

    feat(user surveys) initial implementation of query to get all user's surveys

--- src/server/FakeSurveyGenerator.API/Controllers/SurveyController.cs
+++ src/server/FakeSurveyGenerator.API/Controllers/SurveyController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using FakeSurveyGenerator.Application.Common.Exceptions;
 using FakeSurveyGenerator.Application.Surveys.Commands.CreateSurvey;
 using FakeSurveyGenerator.Application.Surveys.Queries.GetSurveyDetail;
+using FakeSurveyGenerator.Application.Surveys.Queries.GetUserSurveys;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -26,6 +27,21 @@ namespace FakeSurveyGenerator.API.Controllers
         public async Task<IActionResult> GetSurvey(int id, CancellationToken cancellationToken)
         {
             var result = await Mediator.Send(new GetSurveyDetailQuery(id), cancellationToken);
+
+            return FromResult(result);
+        }
+
+        /// <summary>
+        /// Retrieves all Surveys created by the current user.
+        /// </summary>
+        /// <param name="cancellationToken">Automatically set by ASP.NET Core</param>
+        /// <returns>The current user's SurveyModels</returns>
+        /// <response code="200">Returns all SurveyModels for the current user</response> 
+        [HttpGet("user")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetUserSurveys(CancellationToken cancellationToken)
+        {
+            var result = await Mediator.Send(new GetUserSurveysQuery(), cancellationToken);
 
             return FromResult(result);
         }
---
+++ src/server/FakeSurveyGenerator.Application/Surveys/Queries/GetUserSurveys/GetUserSurveysQueryHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CSharpFunctionalExtensions;
+using FakeSurveyGenerator.Application.Common.Errors;
+using FakeSurveyGenerator.Application.Common.Identity;
+using FakeSurveyGenerator.Application.Common.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace FakeSurveyGenerator.Application.Surveys.Queries.GetUserSurveys
+{
+    public sealed class GetUserSurveysQueryHandler : IRequestHandler<GetUserSurveysQuery, Result<List<UserSurveyModel>, Error>>
+    {
+        private readonly IUserService _userService;
+        private readonly ISurveyContext _surveyContext;
+        private readonly IMapper _mapper;
+
+        public GetUserSurveysQueryHandler(IUserService userService, ISurveyContext surveyContext, IMapper mapper)
+        {
+            _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+            _surveyContext = surveyContext ?? throw new ArgumentNullException(nameof(surveyContext));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<Result<List<UserSurveyModel>, Error>> Handle(GetUserSurveysQuery request,
+            CancellationToken cancellationToken)
+        {
+            var userInfo = await _userService.GetUserInfo(cancellationToken);
+
+            var surveyOwner =
+                await _surveyContext.Users.FirstAsync(user => user.ExternalUserId == userInfo.Id,
+                    cancellationToken);
+
+            var surveys = await _surveyContext.Surveys
+                .Include(s => s.Options)
+                .Include(s => s.Owner)
+                .Where(s => s.Owner.Id == surveyOwner.Id)
+                .ProjectTo<UserSurveyModel>(_mapper.ConfigurationProvider)
+                .ToListAsync(cancellationToken);
+
+            return Result.Success<List<UserSurveyModel>, Error>(surveys);
+        }
+    }
+}
