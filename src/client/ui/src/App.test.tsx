237a237111acac0dfa79a1fa5d272f3f0a4bca1e
Marcel Michau <me@marcelmichau.dev>
2021-03-12 16:31:03 +0200

    chore(ui) bring ui in line with latest changes from create-react-app

--- src/client/ui/src/App.test.tsx
+++ src/client/ui/src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test("Renders Fake Survey Generator Title", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Fake Survey Generator/i);
+    expect(linkElement).toBeInTheDocument();
 });
--- src/client/ui/src/index.tsx
+++ src/client/ui/src/index.tsx
@@ -2,25 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/main.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
 import config from "./auth_config.json";
 
 ReactDOM.render(
-    <Auth0Provider
-        domain={config.domain}
-        clientId={config.clientId}
-        redirectUri={window.location.origin}
-        audience={config.audience}
-        useRefreshTokens={true}
-        cacheLocation="localstorage"
-    >
-        <App />
-    </Auth0Provider>,
+    <React.StrictMode>
+        <Auth0Provider
+            domain={config.domain}
+            clientId={config.clientId}
+            redirectUri={window.location.origin}
+            audience={config.audience}
+            useRefreshTokens={true}
+            cacheLocation="localstorage"
+        >
+            <App />
+        </Auth0Provider>
+    </React.StrictMode>,
     document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
