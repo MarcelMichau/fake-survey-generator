786405bdad6b02a00583767bb31570c5fb08ddd1
Marcel Michau <7761114+MarcelMichau@users.noreply.github.com>
2026-04-23 07:58:07 +0200

    fix: remove unnecessary host and port configuration from Vite server settings

--- src/client/ui/vite.config.ts
+++ src/client/ui/vite.config.ts
@@ -5,11 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		host: true,
-		port: Number.parseInt(process.env.PORT ?? "5173"),
 		proxy: {
 			"/api": {
 				target: process.env.API_HTTPS || process.env.API_HTTP,
+				changeOrigin: true
 			},
 		},
 	},
