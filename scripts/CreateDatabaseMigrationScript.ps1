3be0d07cf16ad760a5ae8c5e564b56211dcc9977
Marcel Michau <7761114+MarcelMichau@users.noreply.github.com>
2026-09-11 17:42:36 +0200

    Remove obsolete database migration and app registration scripts

--- scripts/CreateDatabaseMigrationScript.ps1
+++
@@ -1,3 +0,0 @@
-dotnet tool restore
-Set-Location src/server/FakeSurveyGenerator.Application
-dotnet dotnet-ef migrations script -o DbMigrationScript.sql -i
--- scripts/CreateManagedIdentitySqlUser.sql
+++
@@ -1,8 +0,0 @@
-DECLARE @managedIdentityName nvarchar(128) = '$(ManagedIdentityName)';
-
-IF NOT EXISTS (SELECT [name] FROM [sys].[database_principals] WHERE [name] = @managedIdentityName)
-CREATE USER [$(ManagedIdentityName)] FROM EXTERNAL PROVIDER;
-GO
-ALTER ROLE [db_datareader] ADD MEMBER [$(ManagedIdentityName)];
-ALTER ROLE [db_datawriter] ADD MEMBER [$(ManagedIdentityName)];
-GO
